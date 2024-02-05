0e9be6ac1e65033d15c4bdc22927c74a47db92c9
AnaByo <anaalice1224@outlook.com>
2024-02-05 19:13:04 -0300

    eu espero que funcione

---
+++ src/components/button/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div className='w-20 h-20 bg-black'>
+      <h1 className='color'>PEGAAAAAAAA</h1>
+    </div>
+  )
+}
